df749ad296e1325a0001797470f2433e267fbfb0
OrangIndoAseli <fathuryp@gmail.com>
2025-04-02 00:10:10 +0700

    koneksi done

--- config/database.php
+++ config/database.php
@@ -1,19 +1,18 @@
 <?php
 class Database {
-    private static $host = "localhost";
-    private static $user = "root";
-    private static $password = "";
-    private static $dbname = "inventory_db";
-    private static $conn;
+    private $host = "localhost";
+    private $db_name = "sortiran";
+    private $username = "root";
+    private $password = "";
+    private $conn;
 
-    public static function connect() {
-        if (!isset(self::$conn)) {
-            self::$conn = new mysqli(self::$host, self::$user, self::$password, self::$dbname);
-            if (self::$conn->connect_error) {
-                die("Koneksi gagal: " . self::$conn->connect_error);
-            }
+    public function connect() {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        
+        if ($this->conn->connect_error) {
+            die("Koneksi gagal: " . $this->conn->connect_error);
         }
-        return self::$conn;
+        return $this->conn;
     }
 }
 ?>
